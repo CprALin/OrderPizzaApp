db0d3b570c47013d383f72ce49fd399cdf30298a
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-07-31 18:07:53 +0300

    #

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -15,19 +15,33 @@ const cartSlice = createSlice({
     initialState,
     reducers : {
         addItem(state,action){
-
+            // payload = newItem
+            state.cart.push(action.payload);
         },
         deleteItem(state,action){
-
+            // payload = pizzaId
+            state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
         },
         increaseItemQuantity(state,action){
-
+            //payload = pizzaId
+            const item = state.cart.find((item) => item.pizzaId === action.payload);
+            
+            item.quantity++;
+            item.totalPrice = item.quantity * item.unitPrice;
         },
         decreaseItemQuantity(state,action){
-
+            //payload = pizzaId
+            const item = state.cart.find((item) => item.pizzaId === action.payload);
+            
+            item.quantity--;
+            item.totalPrice = item.quantity * item.unitPrice;
         },
-        clearCart(state,action){
-
+        clearCart(state){
+            state.cart = [];
         }
     }
 })
+
+export const { addItem , deleteItem , increaseItemQuantity , decreaseItemQuantity , clearCart } = cartSlice.actions; 
+
+export default cartSlice.reducers;
