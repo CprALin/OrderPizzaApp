605e10fe6280641f771f0ea0fc799e2cc0127b5f
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-08-03 12:42:46 +0300

    #

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -15,7 +15,8 @@ export default function Button({children , disabled , to , type , onClick}) {
        tracking-wide rounded-full hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300 
        focus:outline-none focus:text-stone-800 focus:ring focus:ring-stone-200 focus:ring-offset-2 
        disabled:cursor-not-allowed py-3 px-4 md:px-6 md:py-4`,
-    small : base + ` px-4 py-2.5 md:px-6 md:py-3.5 text-xs`
+    small : base + ` px-4 py-2.5 md:px-6 md:py-3.5 text-xs`,
+    round : base + ` px-2.5 py-1 md:px-3.5 md:py-2 text-sm`
   }
 
   if(to) return <Link className={styles[type]} to={to}>{children}</Link>
